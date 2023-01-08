bd7a9904dd9d195de0840753912b916c33dd36e3
Antoine Droyer <antoine.droyer@gmail.com>
2023-01-08 13:40:01 +0100

    #3

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import mkcert from 'vite-plugin-mkcert'
+// import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { https: true },
-  plugins: [vue(), mkcert()]
+  plugins: [vue()]
 })
